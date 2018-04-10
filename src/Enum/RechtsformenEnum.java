745ca2f237a8806db2ce1dfa8e569923cf3554e5
Marius <marius.berlinghof@stud.hs-mannheim.de>
2018-04-10 09:14:15 +0200

    Berlinghof

---
+++ src/Enum/RechtsformenEnum.java
@@ -0,0 +1,14 @@
+package Enum;
+
+public enum RechtsformenEnum {
+	A,B,C;
+	
+	public xxx methode () {
+		return xxx.D;
+	}
+	
+	public enum xxx{
+		D,E,F;
+	}
+	
+}
---
+++ src/Enum/Test.java
@@ -0,0 +1,12 @@
+package Enum;
+
+public class Test {
+	public static void main (String[] args) {
+		RechtsformenEnum.xxx x = RechtsformenEnum.A.methode();
+		System.out.println(x);
+		
+		RechtsformenEnum.xxx y = RechtsformenEnum.xxx.F;
+		System.out.println(y);
+	}
+
+}
