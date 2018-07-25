4e3d469d1e306f69ba760bb6d2fb37a68702e287
mariu <mariu@Marius-Surface.fritz.box>
2018-07-25 12:08:14 +0200

    Aufgabe 0102 fertig

---
+++ src/Aufgabe_0102/Kunde/Käufer.java
@@ -0,0 +1,18 @@
+package Aufgabe_0102.Kunde;
+
+import Aufgabe_0102.YourPhone.Produkt;
+
+public class Käufer {
+	private Produkt[] bestellKatalog = null;
+	public Käufer (Produkt[] bestellKatalog) {
+		this.bestellKatalog = bestellKatalog;
+	}
+	
+	public String  produktInfosAnfragen() {
+		String ausgabe = "Name\t|\tBeschreibung:\t|\tLieferzeit:\t|\tPreis:\n";
+		for (Produkt t: bestellKatalog) {
+			ausgabe += t.getBezeichnung() + "\t\t" + t.getBeschreibung() + "\t" + t.getLieferZeitInTagen() + "\t\t\t" + t.getPreisUVP() + "\n";
+		}
+		return ausgabe;
+	}
+}
